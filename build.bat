a949995287c5d5b421755b7dd947abf3aaac58e1
Tom Whittaker <whittaker@wisc.edu>
2014-08-06 13:41:26 -0500

    fix class path, include manifest file

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @setlocal
 @echo Make the AreaToNetCDF JAR file....
-set classpath=\src\javacvs;.\;c:\repos\visad\visad.jar
+set classpath=.\;c:\repos\visad\visad.jar
 javac *.java
 
 pause ************  Now Ready to make the JAR file...
